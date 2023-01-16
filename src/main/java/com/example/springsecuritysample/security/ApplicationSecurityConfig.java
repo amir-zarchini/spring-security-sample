9ff1ef5f2e2fc73fe074404eb833d29372a76c25
amir-zarchinie <amzarchini@gmail.com>
2023-01-16 13:58:57 +0330

    add configure security file. closes #3

---
+++ src/main/java/com/example/springsecuritysample/security/ApplicationSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.springsecuritysample.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and().httpBasic();
+        return http.build();
+    }
+}
