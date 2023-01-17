047c3986530a84abf1b37b81034ac14db6e700e4
amir-zarchinie <amzarchini@gmail.com>
2023-01-17 14:02:27 +0330

    disable csrf add hasAuthority method closes #9 closes #10

--- src/main/java/com/example/springsecuritysample/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecuritysample/security/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.springsecuritysample.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static com.example.springsecuritysample.security.ApplicationUserPermission.*;
 import static com.example.springsecuritysample.security.ApplicationUserRole.*;
 
 @Configuration
@@ -28,6 +30,10 @@ public class ApplicationSecurityConfig{
                 .authorizeHttpRequests()
                 .requestMatchers("/**", "index", "/css/*", "/js/*", "api/**").permitAll()
                 .requestMatchers("api/student/*").hasRole(STUDENT.name())
+                .requestMatchers(HttpMethod.DELETE,"api/management/*").hasAnyAuthority(COURSE_WRITE.name())
+                .requestMatchers(HttpMethod.POST,"api/management/*").hasAnyAuthority(COURSE_WRITE.name())
+                .requestMatchers(HttpMethod.PUT,"api/management/*").hasAnyAuthority(COURSE_WRITE.name())
+                .requestMatchers("api/management/*").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and().httpBasic();
