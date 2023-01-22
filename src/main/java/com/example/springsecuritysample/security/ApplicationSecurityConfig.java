58a6cd6d0fd6d9d3d3d5ca04abb604ed1c760b24
amir-zarchinie <amzarchini@gmail.com>
2023-01-22 16:23:05 +0330

    add logout closes #18

--- src/main/java/com/example/springsecuritysample/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecuritysample/security/ApplicationSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
@@ -40,7 +41,15 @@ public class ApplicationSecurityConfig{
                 .defaultSuccessUrl("/courses",true)
                 .and()
                 .rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(15))
-                .key("secured");
+                .key("secured")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID", "remember-me")
+                .logoutSuccessUrl("/login");
         return http.build();
     }
 
